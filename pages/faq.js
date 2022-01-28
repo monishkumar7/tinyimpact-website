d34ba4b229e391067b0f7a5270c74382ddb9752e
monishkumar7 <monishkumar1154@gmail.com>
2022-01-28 07:37:45 -0500

    fix title and favicon

--- pages/faq.js
+++ pages/faq.js
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import FAQContent from '../components/FAQContent';
 
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>tinyimpact | FAQ </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <FAQContent />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import HomeContent from '../components/HomeContent';
 
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>tinyimpact</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <HomeContent />
     </Layout>
   );

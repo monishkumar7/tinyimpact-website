5ada02e564c80bd55157f62a8e1a9d06a0fb32ac
monishkumar7 <monishkumar1154@gmail.com>
2022-02-01 06:13:17 -0500

    update Discord link

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="flex justify-center py-8 px-4">
       <a
-        href="http://discord.gg/0ZjqQZK"
+        href="https://discord.gg/d6JHyWpdDe"
         target="_blank"
         rel="noopener noreferrer"
         className="text-gray-300 bg-gray-700 rounded px-4 py-2 flex items-center space-x-2"
